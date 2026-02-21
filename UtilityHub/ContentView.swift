e270332c0a9554fbff0397f5b26cffae899f41ab
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2026-02-21 14:52:58 +0530

    Initial Commit

---
+++ UtilityHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UtilityHub
+//
+//  Created by utkarsh mishra on 21/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UtilityHub/UtilityHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityHubApp.swift
+//  UtilityHub
+//
+//  Created by utkarsh mishra on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct UtilityHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
